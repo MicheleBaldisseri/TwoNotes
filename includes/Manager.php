944438909ea9661e3210cb4dc6b4e58b289e56ea
Michele <michele.baldisseri@studenti.unipd.it>
2020-12-05 09:38:53 +0100

    fix tutti i link, css altre pagine, tolti br, aggiunte breadcrumbs in giro e menu

--- includes/Manager.php
+++ includes/Manager.php
@@ -19,8 +19,8 @@ class Manager{
     
     public function getPostList(){
         $select = "  SELECT * 
-                    FROM post
-                    ORDER BY DataOra DESC";
+                    FROM Post
+                    ORDER BY dataOra DESC";
 
         $query = $this->dbconnection->query($select);
 
@@ -43,10 +43,10 @@ class Manager{
     private function printPost($post){
 		$string ='
 				<li>
-					<a href="post.php?articleID='.$post['ID_post'].'">
-						<h3 class="articleTitle">' . stripslashes($post['Titolo']) . '</h3>
-                        <p class="description">' . stripslashes($post['Contenuto']) . '</p>
-                        <p class="user">' . stripslashes($post['Utente']) . '</p>
+					<a href="post.php?articleID='.$post['postID'].'">
+						<h3 class="articleTitle">' . stripslashes($post['titolo']) . '</h3>
+                        <p class="description">' . stripslashes($post['contenuto']) . '</p>
+                        <p class="user">' . stripslashes($post['utente']) . '</p>
 					</a>
                 </li>';
         return $string;
---
+++ php/contatti.php
@@ -0,0 +1,7 @@
+<?php
+
+    $paginaHTML = file_get_contents('../views/contatti.html');
+
+    echo $paginaHTML;
+
+?>
