ef357d41e9bfb0a79ec36987631d7d3c06be57bb
MatthewBalzan <matthewb487@gmail.com>
2020-12-05 17:37:22 +0100

    gestione errore login nella pagina di login

--- php/login.php
+++ php/login.php
@@ -1,6 +1,14 @@
 <?php
-
+    session_start();
     $paginaHTML = file_get_contents('../views/login.html');
+    
+    if(isset($_SESSION['loginError'])){
+        $string = '<p id="erroreLogin">'.$_SESSION['loginError'].'</p>';
+        $paginaHTML = str_replace("<ERRORELOGIN />", $string, $paginaHTML);
+        unset($_SESSION['loginError']);
+    }else{
+        $paginaHTML = str_replace("<ERRORELOGIN />", '', $paginaHTML);
+    }
 
     echo $paginaHTML;
 
