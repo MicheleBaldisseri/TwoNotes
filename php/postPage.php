a8ebab535271fa3fec354ee1a72854e388656faa
Michele <michele.baldisseri@studenti.unipd.it>
2020-12-30 14:38:06 +0100

    print registrazione, newPost, contatti, aboutUs

--- php/postPage.php
+++ php/postPage.php
@@ -33,7 +33,7 @@ $paginaHTML = str_replace("<MESSAGGIOAUTENTICAZIONE/>", $messaggioAutenticazione
 
 $success = '';
 if(isset($_SESSION['success'])){
-	$success = '<div id="success_div" class="round_div shadow-div">'.$_SESSION['success'].'</div>';
+	$success = '<div id="success_div" class="round_div shadow-div"><p>'.$_SESSION['success'].'</p></div>';
 	unset($_SESSION['success']);
 }
 $paginaHTML = str_replace("<SUCCESS/>", $success, $paginaHTML);
