0ba0f5ed78d7c3abbcfa91ae1ebd9211e14405aa
Michele <michele.baldisseri@studenti.unipd.it>
2021-01-03 18:24:02 +0100

    fix js

--- js/checkPost.js
+++ js/checkPost.js
@@ -1,9 +1,9 @@
 var dettagli_form = {
 
-    "title": [/^[\w(#$%&=!)]{4,20}$/, "Sono ammesssi numeri, lettere e i simboli #,$,%,&,=,! da 5 a 20 caratteri"],
-    "myfile": [/^[\w-]+\.(jpg|png|gif){1}$/, "Immagine troppo grande e/o non nel formato richiesto"],
-    "altImmagine": [/^(([(a-z)(A-Z)(àèìòù)]+[,.]?[\s]?|[a-zA-Z]+['-]?)+){2,50}$/, "Sono ammesse solo lettere fino a 50 caratteri"],
-    "content": [/^[\w(#$%&=!)]{4,20}$/, "Sono ammesssi numeri, lettere e i simboli #,$,%,&,=,! da 5 a 20 caratteri"],
+    "title": [/^[\w\s(#$%&=!)]{2,20}$/, "Sono ammesssi numeri, lettere e i simboli #,$,%,&,=,! da 2 a 20 caratteri"],
+    "myfile": "Immagine con dimensione troppo grande e/o in un formato non consentito",
+    "altImmagine": [/^[(a-z)(A-Z)(àèìòù)\s]{5,75}$/, "Aggiungere una descrizione. Sono ammesse solo lettere da 5 fino a 75 caratteri"],
+    "content": [/^[\w\s\[\]\/(<>#$%&=!€)]{5,1000}$/, "Sono ammesssi numeri, lettere e i simboli #,$,%,&,=,! e minimo 5 caratteri"],
 }
 
 function mostraErrore(input) {
@@ -11,24 +11,73 @@ function mostraErrore(input) {
     var elemento = document.createElement("strong");
     elemento.className = "errori"; //classe degli errori
        
-    elemento.appendChild(document.createTextNode(dettagli_form[input.id][1])); 
+    if(input == "myfile") //immagine non valida
+        elemento.appendChild(document.createTextNode(dettagli_form[input.id]));    
+    else //tutti gli altri casi
+        elemento.appendChild(document.createTextNode(dettagli_form[input.id][1])); 
 
     var p = input.parentNode; //span 
     p.appendChild(elemento);
 }
 
 function validateCampo(input){
-    
+
     var text = input.value;
     var regex= dettagli_form[input.id][0];
 
-    if(text.search(regex) != 0) {
+    if(input.id == "myfile"){
+        if(document.getElementById("myfile").value != ""){ //se l'immagine è caricata
+            
+            //controllo dimensione
+            var inputImage = document.getElementById("myfile");
+            var image = inputImage.files[0];
+
+            if(image.size > 500000){ //maggiore 500kb
+                mostraErrore(input);
+                return false;
+            }
+
+            //controllo formato
+            var allowedExtension = ['jpeg', 'jpg', 'png', 'gif'];
+            var fileExtension = inputImage.value.split('.').pop().toLowerCase();
+            
+            var isValidFile = false;
+
+            for(var extension in allowedExtension) {
+                if(fileExtension === allowedExtension[extension]) {
+                    isValidFile = true; 
+                }
+            }
+            //formato non consentito
+            if(!isValidFile) {
+                mostraErrore(input);
+                return false;
+            }
+            //tutto ok
+            return isValidFile;
+        }
+        else{ //immagine non caricata, nessun controllo
+            return true;
+        } 
+    }
+    else if(input.id == "altImmagine"){
+        if(document.getElementById("myfile").value != ""){ //se l'immagine è caricata
+            if(text.search(regex) != 0){
+                mostraErrore(input);
+                return false;
+            }else{
+                return true;
+            }
+        }
+        else return true;
+    }
+    else if(text.search(regex) != 0) {  //titolo e contenuto
         //-1 se non l'ha trovata altrimenti ritorna la posizione dove inizia
         mostraErrore(input);
         return false;
     }else{
         return true;
-    }
+    } 
 }
 
 function validateForm(){
@@ -51,4 +100,3 @@ function validateForm(){
     }
     return corretto;
 }
-
---
+++ js/lockTextarea.js
@@ -0,0 +1,48 @@
+var dettagli_form = {
+
+    "username": [ /^[\.\w-]{2,20}$/, '<span xml:lang="en">Username</span> inserito non valido'],
+    "psw": [/^[\w(#$%&=!)]{4,20}$/, '<span xml:lang="en">Password</span> inserita non valida']
+}
+
+function mostraErrore(input) {
+
+    var elemento = document.createElement("strong");
+    elemento.className = "errori"; //classe degli errori
+    elemento.appendChild(document.createTextNode(dettagli_form[input.id][1])); //errore
+
+    var p = input.parentNode; //ovvero lo span aggiunto
+    p.appendChild(elemento);
+}
+
+function validateCampo(input){
+    var regex= dettagli_form[input.id][0];
+    var text = input.value;
+    if(text.search(regex) != 0) {
+        //-1 se non l'ha trovata altrimenti ritorna la posizione dove inizia
+        mostraErrore(input);
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validateForm(){
+    //pulisco eventuali errori precedenti
+    for (var key in dettagli_form){
+        
+        var parent = document.getElementById(key).parentNode; //recupero lo span
+
+        if(parent.children.length == 2){ //array che contiene i figli 
+            //il primo figlio è l'input, il secondo se presente è lo strong con l'errore
+            parent.removeChild(parent.children[1]);
+        }
+    }  
+
+    var corretto = true;
+    for (var key in dettagli_form){
+        var input = document.getElementById(key);
+        var risultato = validateCampo(input);
+        corretto = corretto && risultato;
+    }
+    return corretto;
+}
